f882c37afd5045468afaaa2a2fe5cfbf65a23877
Lewis <lewis.murgor@student.moringaschool.com>
2022-03-13 22:54:33 +0300

    create forms

--- app/main/forms.py
+++ app/main/forms.py
@@ -2,7 +2,22 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField,RadioField,SelectField
 from wtforms.validators import InputRequired
 
+class CommentForm(FlaskForm):
+
+    comment = TextAreaField('Comment')
+    submit = SubmitField('Submit')
+
 class UpdateProfile(FlaskForm):
     
     bio = TextAreaField('Tell us about you.',validators = [InputRequired()])
     submit = SubmitField('Submit')
+
+class BlogForm(FlaskForm):
+
+    title = StringField('Title', validators=[InputRequired()])
+    text = TextAreaField('Add pitch', validators=[InputRequired()])
+    submit = SubmitField('Submit')
+
+class SubscriptionForm(FlaskForm):
+
+    submit = SubmitField('Subscribe')
