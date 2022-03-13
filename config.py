02ab5c7004c84dec57238105a6d897d1e9b6cc15
Lewis <lewis.murgor@student.moringaschool.com>
2022-03-13 12:35:25 +0300

    create configuration classes

---
+++ config.py
@@ -0,0 +1,36 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://lewis:lewis29@localhost/personalblog'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
