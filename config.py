45bf93a3f28ad17cd635c74a2e56d2aa9dd9dbab
Lewis <lewis.murgor@student.moringaschool.com>
2022-03-14 22:09:34 +0300

    deploy to heroku

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://lewis:lewis29@localhost/personalblog'
     DEBUG = True
 
 config_options = {
